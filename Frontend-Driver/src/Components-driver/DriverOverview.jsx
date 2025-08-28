bda7cb7de9523c3b662bf51d9904b95df56a6072
Azeemhaleem <157594820+Azeemhaleem@users.noreply.github.com>
2025-08-28 09:39:00 +0530

    some changes

--- Frontend-Driver/src/Components-driver/DriverOverview.jsx
+++ Frontend-Driver/src/Components-driver/DriverOverview.jsx
@@ -1,16 +1,32 @@
 import React, { useEffect, useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import api from '../api/axios';
+import { useNavigate } from "react-router-dom";
 
-// For development/testing only (set token in localStorage)
-localStorage.setItem('token', '22|rltHcXuNiFu5sbTri43YCfA5173PsHWNiqjbFjKd64779309');
 
 const DriverOverview = () => {
- 
+
   const [fines, setFines] = useState([]);
   const [selectedFines, setSelectedFines] = useState([]);
   const [hoveredFine, setHoveredFine] = useState(null);
-  const [loading, setLoading] = useState(true);
+  const token = localStorage.getItem('token');
+  const [unPaidFines, setUnPaidFines] = useState([]);
+  const [resentPayments, setResentPayments] = useState([]);
+  const [notifications, setNotifications] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!token) {
+      navigate('/login');
+    } else {
+      fetchUnpaidFines();
+      fetchRecentPayments();
+      fetchNotifications();
+    }
+  }, [navigate, token]);
+
 
   // Fetch fines from backend on component mount
   useEffect(() => {
@@ -35,11 +51,15 @@ const DriverOverview = () => {
   };
 
   // Select or deselect all fines
-  const toggleSelectAll = () => {
-    setSelectedFines(
-      fines.length === selectedFines.length ? [] : fines.map((fine) => fine.id)
-    );
-  };
+  // const toggleSelectAll = () => {
+  //   setSelectedFines(
+  //     fines.length === selectedFines.length ? [] : fines.map((fine) => fine.id)
+  //   );
+  // };
+
+  const payFines = () => {
+    navigate('/DriverPayment');
+  }
 
   // Handler for pay button
   const handlePay = async () => {
@@ -56,13 +76,73 @@ const DriverOverview = () => {
       if (response.status === 200) {
         alert('Payment successful!');
         // Refresh fines after payment to update status
-        const updatedFines = await api.get('/get-my-fines');
-        setFines(updatedFines.data);
+        fetchUnpaidFines();
         setSelectedFines([]);
       }
     } catch (error) {
       console.error('Payment failed:', error.response?.data || error.message);
       alert('Payment failed. Please try again.');
+    }
+  };
+
+  const handleLink = () =>{
+    navigate('/DriverMyFines');
+  }
+
+  const fetchUnpaidFines = async () => {
+    try {
+      setLoading(true);
+      setError(null);
+      const response = await api.get('/get-all-unpaid-fines', {
+        headers: { 'Authorization': `Bearer ${token}` }
+      });
+
+      const data = Array.isArray(response?.data?.data) ? response.data.data : [];
+      setUnPaidFines(data);
+    } catch (err) {
+      console.error("Failed to fetch unPaidFines", err);
+      setError("Failed to load unPaidFines. Please try again.");
+      setUnPaidFines([]);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const fetchRecentPayments = async () => {
+    try {
+      setLoading(true);
+      setError(null);
+      const response = await api.get('/get-recently-paid-fines', {
+        headers: { 'Authorization': `Bearer ${token}` }
+      });
+
+      const data = Array.isArray(response?.data?.data) ? response.data.data : [];
+      setResentPayments(data);
+    } catch (err) {
+      console.error("Failed to fetch unPaidFines", err);
+      setError("Failed to load unPaidFines. Please try again.");
+      setResentPayments([]);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const fetchNotifications = async () => {
+    try {
+      setLoading(true);
+      setError(null);
+      const response = await api.get('/driver/notifications/for-today', {
+        headers: { 'Authorization': `Bearer ${token}` }
+      });
+
+      const data = Array.isArray(response?.data?.data) ? response.data.data : [];
+      setResentPayments(data);
+    } catch (err) {
+      console.error("Failed to fetch unPaidFines", err);
+      setError("Failed to load unPaidFines. Please try again.");
+      setNotifications([]);
+    } finally {
+      setLoading(false);
     }
   };
 
@@ -75,16 +155,40 @@ const DriverOverview = () => {
         marginLeft: window.innerWidth < 576 ? '2rem' : '3rem',
       }}
     >
+
       <div className="bg-white rounded-4 shadow-sm p-4 mb-4">
         <div className="d-flex justify-content-between align-items-center mb-3">
           <h4 className="fw-bold m-0">Overview Fines</h4>
           <button
-            className="btn btn-dark px-3 py-1"
-            style={{ width: 'fit-content', borderRadius: '10px', fontSize: '0.85rem' }}
-            onClick={toggleSelectAll}
-          >
-            Select All
+            className="btn btn-dark px-2 py-2"
+            style={{ width: '150px', borderRadius: '10px', fontSize: '0.88rem' }}
+            onClick={payFines}>
+            Pay All Fines
           </button>
+        </div>
+
+        <div className="bg-white rounded-4 p-3">
+          <div className="row">
+            <div className="col-md-12 mb-3">
+              {unPaidFines.length === 0 && !loading ? (
+                  <div className="bg-primary-subtle rounded-4 p-2 mb-2">No Recent Fines.</div>
+              ) : (
+                  unPaidFines.map((item) => (
+                      item.length < 4 && (
+                          <div key={item.id} className="d-flex mb-2">
+                            <div className="d-flex bg-primary-subtle rounded-4 px-3 py-2">
+                              {item.fine_name}
+                            </div>
+                              <button className="d-flex btn btn-dark mx-2 rounded-3" style={{ width: 'fit-content' }} onClick={handlePay}>
+                                Pay
+                              </button>
+                          </div>
+
+                      )
+                  ))
+              )}
+            </div>
+          </div>
         </div>
 
         {fines.map((fine) => (
@@ -94,45 +198,36 @@ const DriverOverview = () => {
           >
             <span className="fw-semibold">{fine.name || 'Unknown Fine'}</span>
 
-            <div
-              className="d-flex align-items-center position-relative"
-              style={{ gap: '1rem' }}
-            >
-              <span
-                className="fs-4"
-                style={{ cursor: 'pointer' }}
-                onMouseEnter={() => setHoveredFine(fine.id)}
-                onMouseLeave={() => setHoveredFine(null)}
-              >
-                ⋮
+            <div className="d-flex align-items-center position-relative" style={{ gap: '1rem' }}>
+              <span className="fs-4" style={{ cursor: 'pointer' }} onMouseEnter={() => setHoveredFine(fine.id)} onMouseLeave={() => setHoveredFine(null)}>⋮
               </span>
 
-              {hoveredFine === fine.id && (
-                <div
-                  className="position-absolute p-2 rounded-3 shadow-sm bg-white border"
-                  style={{
-                    top: '30px',
-                    right: '0',
-                    zIndex: 10,
-                    minWidth: '200px',
-                    fontSize: '0.9rem',
-                  }}
-                >
-                  <div>
-                    <strong>Violation:</strong> {fine.violation || 'N/A'}
-                  </div>
-                  <div>
-                    <strong>Date:</strong> {fine.date || 'N/A'}
-                  </div>
-                  <div>
-                    <strong>Fine Amount:</strong> {fine.amount || 'N/A'}
-                  </div>
-                  <div>
-                    <strong>Status:</strong>{' '}
-                    {fine.paid_at ? 'Paid' : 'Unpaid'}
-                  </div>
-                </div>
-              )}
+              {/*{hoveredFine === fine.id && (*/}
+              {/*  <div*/}
+              {/*    className="position-absolute p-2 rounded-3 shadow-sm bg-white border"*/}
+              {/*    style={{*/}
+              {/*      top: '30px',*/}
+              {/*      right: '0',*/}
+              {/*      zIndex: 10,*/}
+              {/*      minWidth: '200px',*/}
+              {/*      fontSize: '0.9rem',*/}
+              {/*    }}*/}
+              {/*  >*/}
+              {/*    <div>*/}
+              {/*      <strong>Violation:</strong> {fine.violation || 'N/A'}*/}
+              {/*    </div>*/}
+              {/*    <div>*/}
+              {/*      <strong>Date:</strong> {fine.date || 'N/A'}*/}
+              {/*    </div>*/}
+              {/*    <div>*/}
+              {/*      <strong>Fine Amount:</strong> {fine.amount || 'N/A'}*/}
+              {/*    </div>*/}
+              {/*    <div>*/}
+              {/*      <strong>Status:</strong>{' '}*/}
+              {/*      {fine.paid_at ? 'Paid' : 'Unpaid'}*/}
+              {/*    </div>*/}
+              {/*  </div>*/}
+              {/*)}*/}
 
               <input
                 className="form-check-input rounded-circle border-secondary"
@@ -146,15 +241,8 @@ const DriverOverview = () => {
         ))}
 
         <div className="d-flex justify-content-between">
-          <button className="btn btn-dark px-3 py-1" style={{ width: 'fit-content' }}>
+          <button className="btn btn-dark px-3 py-1 rounded-3" style={{ width: 'fit-content' }} onClick={handleLink}>
             View all fines
-          </button>
-          <button
-            className="btn btn-dark px-3 py-1"
-            style={{ width: 'fit-content' }}
-            onClick={handlePay}
-          >
-            Pay
           </button>
         </div>
       </div>
@@ -164,15 +252,59 @@ const DriverOverview = () => {
         <div className="row">
           <div className="col-md-6 mb-3">
             <h6 className="small">Recent Payments</h6>
-            <div className="bg-primary-subtle rounded-4 p-3 mb-2"></div>
-            <div className="bg-primary-subtle rounded-4 p-3 mb-2"></div>
-            <div className="bg-primary-subtle rounded-4 p-3"></div>
+            {/*<div className="bg-primary-subtle rounded-4 p-3 mb-2"></div>*/}
+            {/*<div className="bg-primary-subtle rounded-4 p-3 mb-2"></div>*/}
+            {/*<div className="bg-primary-subtle rounded-4 p-3"></div>*/}
+            {resentPayments.length === 0 && !loading ? (
+                <div className="bg-primary-subtle rounded-4 p-2 mb-2">No Recent Payments.</div>
+                // <div className="d-flex mb-2">
+                //   <div className="d-flex bg-primary-subtle rounded-4 px-3 py-2">
+                //     No Pending Payment Fines in the last 7 days.
+                //   </div>
+                //   <button className="d-flex btn btn-dark mx-2 rounded-3" style={{ width: 'fit-content' }} onClick={handlePay}>
+                //     Pay
+                //   </button>
+                // </div>
+            ) : (
+                resentPayments.map((item) => (
+                    item.length < 4 && (
+                        <div key={item.id} className="d-flex mb-2">
+                          <div className="d-flex bg-primary-subtle rounded-4 px-3 py-2">
+                            {item.fine_name}
+                          </div>
+                        </div>
+
+                    )
+                ))
+            )}
           </div>
           <div className="col-md-6">
             <h6 className="small">Notifications</h6>
-            <div className="bg-primary-subtle rounded-4 p-3 mb-2"></div>
-            <div className="bg-primary-subtle rounded-4 p-3 mb-2"></div>
-            <div className="bg-primary-subtle rounded-4 p-3"></div>
+            {/*<div className="bg-primary-subtle rounded-4 p-3 mb-2"></div>*/}
+            {/*<div className="bg-primary-subtle rounded-4 p-3 mb-2"></div>*/}
+            {/*<div className="bg-primary-subtle rounded-4 p-3"></div>*/}
+            {notifications.length === 0 && !loading ? (
+                <div className="bg-primary-subtle rounded-4 p-2 mb-2">No Notifications.</div>
+                // <div className="d-flex mb-2">
+                //   <div className="d-flex bg-primary-subtle rounded-4 px-3 py-2">
+                //     No Pending Payment Fines in the last 7 days.
+                //   </div>
+                //   <button className="d-flex btn btn-dark mx-2 rounded-3" style={{ width: 'fit-content' }} onClick={handlePay}>
+                //     Pay
+                //   </button>
+                // </div>
+            ) : (
+                notifications.map((item) => (
+                    item.length < 4 && (
+                        <div key={item.id} className="d-flex mb-2">
+                          <div className="d-flex bg-primary-subtle rounded-4 px-3 py-2">
+                            {item.data.message.length > 11 ? item.data.message.slice(0, 10) + '...' : item.data.message}
+                          </div>
+                        </div>
+
+                    )
+                ))
+            )}
           </div>
         </div>
       </div>
--- Frontend-Officers/src/Components-admin/Admin-messages.jsx
+++ Frontend-Officers/src/Components-admin/Admin-messages.jsx
@@ -1,50 +1,296 @@
-import "../Components-Superadmin/styles/driver-style.css"
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCheck, faCircleCheck, faEllipsisVertical, faMessage, faTrash } from '@fortawesome/free-solid-svg-icons';
+import api from "../api/axios.jsx";
+import { useNavigate } from "react-router-dom";
+import "../Components-officer/Officer-styles.css";
 
 
 function AdminMessages() {
+  const token = localStorage.getItem('token');
+  const [notifications, setNotifications] = useState([]);
+  const [showCheckbox, setShowCheckbox] = useState(false);
+  const [selectOptions, setSelectOptions] = useState(false);
+  const [showOptions, setShowOptions] = useState(false);
+  const [selectedMessage, setSelectedMessage] = useState(null); // for modal
+  const navigate = useNavigate();
 
-  const messages = [
-    { msgText: "msg1", Time: "7.00 AM", },
-    { msgText: "msg2", Time: "7.30 AM", },
-    { msgText: "msg3", Time: "8.00 AM", },
-    { msgText: "msg4", Time: "7.15 AM", },
-    { msgText: "msg5", Time: "7.45 AM", },
-    { msgText: "msg6", Time: "9.00 PM", },
-    { msgText: "msg7", Time: "2.00 PM", },
-    { msgText: "msg8", Time: "7.00 AM", },
-    { msgText: "msg9", Time: "7.30 AM", },
-    { msgText: "msg10", Time: "8.00 AM", },
-    { msgText: "msg11", Time: "7.15 AM", },
-    { msgText: "msg12", Time: "7.45 AM", },
-    { msgText: "msg14", Time: "9.00 PM", },
-    { msgText: "msg15", Time: "2.00 PM", },
-  ];
+  // Fetch notifications on load
+  useEffect(() => {
+    if (!token) {
+      navigate('/loginPolice');
+    } else {
+      fetchNotifications();
+    }
+  }, [navigate]);
+
+  const [loading, setLoading] = useState(false);
+
+  const fetchNotifications = async () => {
+    try {
+      setLoading(true);
+      const response = await api.get('/police/notifications/all', {
+        headers: { 'Authorization': `Bearer ${token}` }
+      });
+      if(response.status === 200) {
+        setNotifications(response.data);
+      }
+    } catch (err) {
+      console.error("Failed to fetch notifications", err);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const handleCheckboxClickTodayIssued = () => {
+    setShowCheckbox(!showCheckbox);
+  };
+
+  const toggleSelectMessage = (id) => {
+    setNotifications(prev =>
+        prev.map(msg => msg.id === id ? { ...msg, isSelected: !msg.isSelected } : msg)
+    );
+  };
+
+  const deleteSelectedMessages = async () => {
+    const selectedIds = notifications.filter(msg => msg.isSelected).map(msg => msg.id);
+
+    try {
+      for (const id of selectedIds) {
+        await api.delete('/police/notifications/delete', {
+          data: { notification_id: id },
+          headers: { 'Authorization': `Bearer ${token}` }
+        });
+      }
+
+      setNotifications(prev => prev.filter(msg => !msg.isSelected));
+      setShowCheckbox(false);
+
+    } catch (error) {
+      console.error('Notification delete error:', error.response?.data || error.message);
+      alert("Failed to delete notifications. Please try again later.");
+    }
+  };
+
+  const markSelectedMessages = async () => {
+    const selectedIds = notifications.filter(msg => msg.isSelected).map(msg => msg.id);
+
+    try {
+      for (const id of selectedIds) {
+        const response = await api.put('/police/notifications/mark-as-read',
+            { notification_id: id },  // Send single ID as string
+            {
+              headers: {
+                'Authorization': `Bearer ${token}`,
+                'Content-Type': 'application/json'
+              }
+            }
+        );
+        if(response.status === 200) {
+          console.log("mark as read successfully");
+          window.location.reload();
+        }
+      }
+
+      setNotifications(prev =>
+          prev.map(msg => selectedIds.includes(msg.id) ? { ...msg, read_at: true } : msg)
+      );
+
+    } catch (error) {
+      console.error('Mark as read error:', error.response?.data || error.message);
+      alert(error.response?.data?.message || "Failed to mark notifications as read");
+    }
+  };
+
+  const deleteAllMessages = async () => {
+    try {
+      const response = await api.delete('/police/notifications/delete-all', {
+        headers: { 'Authorization': `Bearer ${token}` }
+      });
+      if(response.status === 200){
+        console.log("All notifications are deleted successfully");
+        setNotifications([]);
+      }
+    } catch (err) {
+      console.error("Error deleting all messages:", err);
+    }
+  };
+
+  const markAllMessages = async () => {
+    try {
+      const response = await api.put('/police/notifications/mark-all-as-read', {}, {
+        headers: { 'Authorization': `Bearer ${token}` }
+      });
+      if(response.status === 200){
+        console.log("All notifications are mark as read successfully");
+        setNotifications(prev => prev.map(msg => ({ ...msg, read_at: true })));
+      }
+
+    } catch (err) {
+      console.error("Error marking all as read:", err);
+    }
+  };
+
+  useEffect(() => {
+    const anySelected = notifications.some(item => item.isSelected);
+    setSelectOptions(anySelected);
+  }, [notifications]);
+
+  const openMessageModal = (msg) => {
+    setSelectedMessage(msg);
+  };
+
+  const closeModal = () => {
+    setSelectedMessage(null);
+  };
 
   return (
-    <>
-    <div className='row'>
-      <div className="search-section container" style={{ backgroundColor: "#d3e2fd",borderRadius: "10px",padding:"0px" }}>
-      <h5 className="fw-bold" style={{margin:"7px"}}>Messages</h5>
-     
- <ul className="list-group list-group-flush" style={{ maxHeight: "300px" ,borderRadius: "10px"}}>
+      <>
+        {selectedMessage && (
+            <div className="modal-overlay" onClick={closeModal}>
+              <div className="modal-content" onClick={(e) => e.stopPropagation()}>
+                <p><strong>Message</strong></p>
+                <div className="warning-box">
+                  <p>{selectedMessage.data.message}</p>
+                </div>
+                <div className="modal-actions">
+                  <button className="btn-cancel" onClick={closeModal}>
+                    Back
+                  </button>
+                  <button className="btn-confirm" onClick={() => {
+                    toggleSelectMessage(selectedMessage.id);
+                    deleteSelectedMessages();
+                    markSelectedMessages();
+                    closeModal();
+                  }}>
+                    Delete Message
+                  </button>
+                </div>
+              </div>
+            </div>
+        )}
 
-{
-  messages.map((item) => (
-<li  className="list-group-item d-flex justify-content-between align-items-center">
-           {item.msgText}
-            <span className="text-muted small">{item.Time}</span>
-          </li>
-  ))
-}
+        <div className='fines-list'>
+          <div className="search-section container" style={{ backgroundColor: "#d3e2fd", borderRadius: "10px", padding: "0px" }}>
+            <div className="d-flex align-content-center">
+              <h5 className="fw-bold d-flex justify-content-start" style={{ margin: "7px" }}>
+                All Notifications
+              </h5>
+              <button
+                  className="me-lg-3 rounded p-2 d-flex justify-content-end align-items-center border-0"
+                  style={{ width: "fit-content", height: "fit-content", backgroundColor: "#d3e2fd" }}
+                  onClick={handleCheckboxClickTodayIssued}
+              >
+                <FontAwesomeIcon icon={faCircleCheck} className="fs-3" />
+              </button>
+              <button
+                  className="me-lg-3 rounded px-3 py-2 d-flex justify-content-end align-items-center border-0"
+                  style={{width: "fit-content", height: "fit-content", backgroundColor: "#d3e2fd"}}
+                  onClick={() => setShowOptions(prev => !prev)}
+              >
+                <FontAwesomeIcon icon={faEllipsisVertical} className="fs-3" />
+              </button>
+            </div>
 
-</ul>
-      
-      </div>
-      
-    </div>
-    </>
-  )
-}
+            <ul className="list-group list-group-flush d-flex" style={{ maxHeight: "75vh", borderRadius: "10px" }}>
+              {notifications.length === 0  ? (
+                      <li className="list-group-item text-center text-muted">
+                        You do not have any notifications.
+                      </li>
+                  ):
+                  (
+                      notifications.map((item) => (
+                          <li key={item.id} className="list-group-item d-flex w-100">
+                            {showCheckbox && (
+                                <input
+                                    type="checkbox"
+                                    checked={item.isSelected || false}
+                                    onChange={() => toggleSelectMessage(item.id)}
+                                    className="me-1"
+                                    style={{ width: "5%" }}
+                                />
+                            )}
+                            <button
+                                className="d-flex justify-content-between w-100 border-0 bg-white text-li"
+                                style={{ textAlign: "start" }}
+                                onClick={() => openMessageModal(item)}
+                            >
+                                            <span className={`d-flex justify-content-start ${item.read_at ? "text-muted" : "text-dark"}`}>
+                                                {item.data.message.length > 50 ? item.data.message.slice(0, 50) + '...' : item.data.message}
+                                            </span>
+                              {(() => {
+                                const createdDate = new Date(item.created_at);
+                                return (
+                                    <span className="text-muted small justify-content-end text-end">
+                                                            {createdDate.toLocaleTimeString()}  {createdDate.toLocaleDateString()}
+                                                        </span>
+                                );
+                              })()}
+                            </button>
+                          </li>
+                      ))
+                  )
+              }
+
+            </ul>
+          </div>
+        </div>
+
+        {showOptions && (
+            <div className='notification-options row'>
+              <div className="search-section container w-25"
+                   style={{backgroundColor: "#d3e2fd", borderRadius: "10px", padding: "0px"}}>
+                <ul className="list-group list-group-flush d-flex"
+                    style={{maxHeight: "75vh", borderRadius: "10px"}}>
+                  {selectOptions ? (
+                      <>
+                        <li className="list-group-item d-flex bg-light">
+                          <button className="d-flex border-0 bg-light w-100" onClick={markSelectedMessages}>
+                                            <span className="d-flex justify-content-start align-content-center mt-1"
+                                                  style={{width: "15%", textAlign: "center"}}>
+                                                <FontAwesomeIcon icon={faMessage} />
+                                            </span>
+                            <span className="d-flex justify-content-start align-content-center">Mark Selected as Read</span>
+                          </button>
+                        </li>
+                        <li className="list-group-item d-flex bg-light">
+                          <button className="d-flex border-0 bg-light w-100" onClick={deleteSelectedMessages}>
+                                            <span className="d-flex justify-content-start align-content-center mt-1"
+                                                  style={{width: "15%", textAlign: "center"}}>
+                                                <FontAwesomeIcon icon={faTrash} />
+                                            </span>
+                            <span className="d-flex justify-content-start align-content-center">Delete Selected</span>
+                          </button>
+                        </li>
+                      </>
+                  ) : (
+                      <>
+                        <li className="list-group-item d-flex bg-light">
+                          <button className="d-flex border-0 bg-light w-100" onClick={markAllMessages}>
+                                            <span className="d-flex justify-content-start align-content-center mt-1"
+                                                  style={{width: "15%", textAlign: "center"}}>
+                                                <FontAwesomeIcon icon={faMessage} />
+                                            </span>
+                            <span className="d-flex justify-content-start align-content-center">Mark All as Read</span>
+                          </button>
+                        </li>
+                        <li className="list-group-item d-flex bg-light">
+                          <button className="d-flex border-0 bg-light w-100" onClick={deleteAllMessages}>
+                                            <span className="d-flex justify-content-start align-content-center mt-1"
+                                                  style={{width: "15%", textAlign: "center"}}>
+                                                <FontAwesomeIcon icon={faTrash} />
+                                            </span>
+                            <span className="d-flex justify-content-start align-content-center">Delete All Notifications</span>
+                          </button>
+                        </li>
+                      </>
+                  )}
+                </ul>
+              </div>
+            </div>
+        )}
+      </>
+  );
+};
 
 export default AdminMessages
